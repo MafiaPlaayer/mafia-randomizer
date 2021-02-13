b666f9eed9bcfedd1e0a81797e20e1482c786ed6
Golnaz <Golnaz@Golnazs-MacBook-Pro-2.local>
2021-02-13 18:08:16 -0500

    initial commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"html/template"
+	"net/http"
+)
+
+var tpl = template.Must(template.ParseFiles("index.html"))
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	tpl.Execute(w, nil)
+}
+
+func randomHandler(w http.ResponseWriter, r *http.Request) {
+	tpl.Execute(w, nil)
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", indexHandler)
+	r.HandleFunc("/random", randomHandler)
+	http.Handle("/", r)
+
+	http.ListenAndServe(":8080", r)
+}
+
